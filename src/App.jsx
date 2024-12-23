20ab64c205301afe0040638c6ffaaf413eea2a22
DS <user@MacBook-Pro.local>
2024-12-23 21:00:05 +0000

    updated code for custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import FetchData from './Components/FetchData'
+import FetchYogaData from './Components/FetchYogaData'
 
 function App() {
   return (
  <>
+ <h1>YOGA INFO</h1>
+ <FetchYogaData/>
+ <br/>
+ <h1>FOOD INFO</h1>
  <FetchData/>
  </>
   )
